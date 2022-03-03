a92f0dea9d3c68e30c8e05db614395a6d6fc3a82
MarcosDS7 <marcossouza.0ds@gmail.com>
2022-03-03 18:12:25 -0300

    Implementação das rotas e pipes

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -5,10 +5,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list/course-list.component';
 import { StarComponent } from './courses/star/star.component';
+import { ReplacePipe } from './pipes/replace.pipe';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './courses/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseListComponent, StarComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavbarComponent,
+    PageNotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'courses', pathMatch: 'full' },
+      { path: 'courses', component: CourseListComponent },
+      { path: '**', component: PageNotFoundComponent },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
